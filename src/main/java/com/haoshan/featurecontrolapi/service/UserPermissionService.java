563ebe16310e7213cb021db15d68c26a18215100
haoshan98 <haoshanw@gmail.com>
2021-04-13 20:25:10 +0800

    refactor

--- src/main/java/com/haoshan/featurecontrolapi/service/UserPermissionService.java
+++ src/main/java/com/haoshan/featurecontrolapi/service/UserPermissionService.java
@@ -2,8 +2,6 @@ package com.haoshan.featurecontrolapi.service;
 
 import com.haoshan.featurecontrolapi.model.UserPermission;
 
-import javax.transaction.Transactional;
-
 public interface UserPermissionService {
 
     UserPermission getUserPermissionByUserIdAndFeatureId(int userId, int featureId);
